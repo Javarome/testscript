0e66e74dd6e5816e7466b777d79eb4a881fa1f0d
javarome <javarome@gmail.com>
2023-11-15 16:26:11 +0100

    fix log levels

--- src/log/DefaultLogger.ts
+++ src/log/DefaultLogger.ts
@@ -3,7 +3,7 @@ import {Logger} from "./Logger"
 const NOP = () => {
 }
 
-const defaultLogLevels = ["info", "warn", "error"]
+const defaultLogLevels = process.env.LOGLEVEL || process.env.LOG_LEVEL|| ["info", "warn", "error"]
 
 export interface LogConsole {
   log(...data: any[]): void
@@ -19,14 +19,18 @@ const defaultConsole = console
 
 export class DefaultLogger implements Logger {
 
-  readonly log = this.logLevels.includes("info") ? (...data: any[]) => this.console.log(this.name + ":", ...data) : NOP
-  readonly debug = this.logLevels.includes("debug") ? (...data: any[]) => this.console.debug(this.name + ":",
-    ...data) : NOP
-  readonly warn = this.logLevels.includes("warn") ? (...data: any[]) => this.console.warn(this.name + ":",
-    ...data) : NOP
-  readonly error = this.logLevels.includes("error") ? (...data: any[]) => this.console.error(this.name + ":",
-    ...data) : NOP
+  readonly log
+  readonly debug
+  readonly warn
+  readonly error
 
   constructor(public name: string, protected console: LogConsole = defaultConsole, protected logLevels = defaultLogLevels) {
+    this.log = this.logLevels.includes("info") ? (...data: any[]) => this.console.log(this.name + ":", ...data) : NOP
+    this.debug = this.logLevels.includes("debug") ? (...data: any[]) => this.console.debug(this.name + ":",
+      ...data) : NOP
+    this.warn = this.logLevels.includes("warn") ? (...data: any[]) => this.console.warn(this.name + ":",
+      ...data) : NOP
+    this.error = this.logLevels.includes("error") ? (...data: any[]) => this.console.error(this.name + ":",
+      ...data) : NOP
   }
 }
